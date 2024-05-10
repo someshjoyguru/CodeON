75e9cdf1f20203e650a5696cba0c2ac12d9cc270
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2024-05-11 03:43:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,24 +12,9 @@ config();
 
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = [
-  'https://nitjsr-cp-portal-lgf6ixq33-someshjoygurus-projects.vercel.app',
-  // Add other allowed origins if needed
-];
-
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // Allow credentials (cookies, authorization headers)
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors({
+  credentials: true,
+}));
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/leaderboard", leaderboardRouter);
