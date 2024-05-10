f1b47eac1ae60f2922b0810867faa053079e2293
Somesh Ghosh <someshghoshjoyguru@gmail.com>
2024-05-11 02:35:39 +0530

    asdasd

--- app.js
+++ app.js
@@ -12,13 +12,7 @@ config();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ['http://localhost:5173','https://achieveit-mocha.vercel.app', 'https://achieve-it-omega.vercel.app'],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+
 
 
 app.use("/api/v1/users", userRouter);
