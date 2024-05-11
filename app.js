11c57c9b29dedae87493d5ff4ec2c02a9504a9f2
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2024-05-11 10:22:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,13 +8,20 @@ import cors from "cors";
 
 export const app = express();
 
-config();
+config({
+  path: "./data/config.env",
+});
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ['http://localhost:5173','https://nitjsr-cp-portal.vercel.app'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/leaderboard", leaderboardRouter);
