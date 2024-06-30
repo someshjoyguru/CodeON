fee334bdeb37f6fd0bc6def43c0931a32aa6951b
Somesh Ghosh <someshghoshjoyguru@gmail.com>
2024-06-30 07:41:24 +0530

    bug fixes

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/features.js";
 import ErrorHandler from "../middlewares/error.js";
-import { cloudinary } from "../app.js";
+import { v2 as cloudinary } from 'cloudinary';
 
 export const login = async (req, res, next) => {
   try {
