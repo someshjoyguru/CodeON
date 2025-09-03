c56156c06b7fdd8240699e5a54d90b5d1871625d
Somesh Ghosh <someshghoshjoyguru@gmail.com>
2025-09-03 10:38:51 +0530

    Update CORS origin to correct production server URL

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173','https://codeon-server.someshghosh.me'],
+    origin: ['http://localhost:5173','https://codeon.someshghosh.me'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
